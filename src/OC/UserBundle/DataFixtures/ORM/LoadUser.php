080473dcf73488caa0ea29dc13c5ca2ffb946795
Rémy Roger <rroger@kaliop.com>
2017-09-22 09:11:56 +0200

    Sécurité et gestion des utilisateurs

---
+++ src/OC/UserBundle/DataFixtures/ORM/LoadUser.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace OC\UserBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use OC\UserBundle\Entity\User;
+
+class LoadUser implements FixtureInterface
+{
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        // Les noms d'utilisateurs à créer
+        $listNames = array('Alexandre', 'Marine', 'Anna');
+
+        foreach ($listNames as $name) {
+            // On crée l'utilisateur
+            $user = new User;
+
+            // Le nom d'utilisateur et le mot de passe sont identiques pour l'instant
+            $user->setUsername($name);
+            $user->setPassword($name);
+
+            // On ne se sert pas du sel pour l'instant
+            $user->setSalt('');
+            // On définit uniquement le role ROLE_USER qui est le role de base
+
+            // On le persiste
+            $manager->persist($user);
+        }
+
+        // On déclenche l'enregistrement
+        $manager->flush();
+    }
+}
